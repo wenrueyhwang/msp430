f903f26f9aaffb349c6ed14bf641b79076e36847
Wen-Ruey Hwang <wrh8609@gmail.com>
2018-01-05 22:54:15 -0500

    The difference between initial and rewite

--- hello.c
+++ hello.c
@@ -4,6 +4,8 @@ int main(void)
 	printf("This is a test for  wen-ruey Hwang \n");
 	printf("Hello ???\n");
 	printf("%s\n", "hello world" );
+	printf("%s\n", "bye world");
+	printf("%s\n", "This is a test for test files");
 	return(0);
 }
 
